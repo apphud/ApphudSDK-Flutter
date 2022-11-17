f7a0607db7fbd3ed43cf7cb38740f56d3623e295
alien <alien1900@bk.ru>
2022-11-17 15:54:58 +0700

    fix(Android): ApphudListener behaviour is not correct when the ApphudPlugin instance is created twice because of Firebase. #38.

--- android/src/main/kotlin/com/apphud/fluttersdk/handlers/ApphudListenerHandler.kt
+++ android/src/main/kotlin/com/apphud/fluttersdk/handlers/ApphudListenerHandler.kt
@@ -10,29 +10,54 @@ import com.apphud.sdk.domain.ApphudSubscription
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
-class ApphudListenerHandler(private val channel: MethodChannel) : MethodChannel.MethodCallHandler,
+class ApphudListenerHandler() : MethodChannel.MethodCallHandler,
     ApphudListener {
     private var isListeningStarted: Boolean = false
+    private var channel: MethodChannel? = null
+    private var userIdCached: String? = null
+    private var detailsCached: List<SkuDetails>? = null
+    private var paywallsCached: List<ApphudPaywall>? = null
+    private var didUserLoad: Boolean = false
+    private var subscriptionsCached: List<ApphudSubscription>? = null
+    private var purchasesCached: List<ApphudNonRenewingPurchase>? = null
+
 
     init {
-        channel.setMethodCallHandler(this)
         Apphud.setListener(this)
+    }
+
+    fun setMethodCallHandler(channel: MethodChannel?) {
+        this.channel?.setMethodCallHandler(null);
+        if (channel == null) {
+            isListeningStarted = false
+            this.channel = null
+        } else {
+            this.channel = channel
+            this.channel!!.setMethodCallHandler(this)
+        }
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
-            "startListening" -> start()
-            "stopListening" -> stop()
+            "startListening" -> {
+                start()
+                result.success(null)
+            }
+            "stopListening" -> {
+                stop()
+                result.success(null)
+            }
         }
-    }
-
-    fun dispose() {
-        isListeningStarted = false
-        channel.setMethodCallHandler(null)
     }
 
     private fun start() {
         isListeningStarted = true
+        userIdCached?.let { v -> apphudDidChangeUserID(v) }
+        detailsCached?.let { v -> apphudFetchSkuDetailsProducts(v) }
+        paywallsCached?.let { v -> paywallsDidFullyLoad(v) }
+        if (didUserLoad) userDidLoad()
+        subscriptionsCached?.let { v -> apphudSubscriptionsUpdated(v) }
+        purchasesCached?.let { v -> apphudNonRenewingPurchasesUpdated(v) }
     }
 
     private fun stop() {
@@ -40,51 +65,57 @@ class ApphudListenerHandler(private val channel: MethodChannel) : MethodChannel.
     }
 
     override fun apphudDidChangeUserID(userId: String) {
+        userIdCached = userId
         if (isListeningStarted) {
-            channel.invokeMethod("didChangeUserID", userId)
+            channel?.invokeMethod("didChangeUserID", userId)
         }
     }
 
     override fun apphudFetchSkuDetailsProducts(details: List<SkuDetails>) {
+        detailsCached = details
         if (isListeningStarted) {
             val resultMap: List<HashMap<String, Any?>> = details.map {
                 it.toMap()
             }
-            channel.invokeMethod("fetchNativeProducts", resultMap)
+            channel?.invokeMethod("fetchNativeProducts", resultMap)
         }
     }
 
     override fun paywallsDidFullyLoad(paywalls: List<ApphudPaywall>) {
+        paywallsCached = paywalls
         if (isListeningStarted) {
             val resultMap = hashMapOf<String, Any?>()
             resultMap["paywalls"] = paywalls.map { paywall -> paywall.toMap() }
-            channel.invokeMethod("paywallsDidFullyLoad", resultMap)
+            channel?.invokeMethod("paywallsDidFullyLoad", resultMap)
         }
     }
 
     override fun userDidLoad() {
+        didUserLoad = true
         if (isListeningStarted) {
             val resultMap = hashMapOf<String, Any?>()
             resultMap["paywalls"] = Apphud.paywalls().map { paywall -> paywall.toMap() }
-            channel.invokeMethod("userDidLoad", resultMap)
+            channel?.invokeMethod("userDidLoad", resultMap)
         }
     }
 
     override fun apphudSubscriptionsUpdated(subscriptions: List<ApphudSubscription>) {
+        subscriptionsCached = subscriptions
         if (isListeningStarted) {
             val resultMap: List<HashMap<String, Any?>> = subscriptions.map {
                 it.toMap()
             }
-            channel.invokeMethod("apphudSubscriptionsUpdated", resultMap)
+            channel?.invokeMethod("apphudSubscriptionsUpdated", resultMap)
         }
     }
 
     override fun apphudNonRenewingPurchasesUpdated(purchases: List<ApphudNonRenewingPurchase>) {
+        purchasesCached = purchases
         if (isListeningStarted) {
             val resultMap: List<HashMap<String, Any?>> = purchases.map {
                 it.toMap()
             }
-            channel.invokeMethod("apphudNonRenewingPurchasesUpdated", resultMap)
+            channel?.invokeMethod("apphudNonRenewingPurchasesUpdated", resultMap)
         }
     }
 }
