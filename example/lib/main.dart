578c01157f7f03b844583bee033de831e057dbec
alien <alien1900@bk.ru>
2024-01-28 17:56:46 +0700

    The simple app based on a StateFullWidget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:apphud_example/src/feature/common/debug_bloc_observer.dart';
 import 'package:apphud_example/src/my_app.dart';
+import 'package:apphud_example/src/my_simple_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   Bloc.observer = DebugBlocObserver();
   runApp(MyApp());
+  // Comment above and uncomment below to use simple app based on a StateFullWidget
+  //runApp(MySimpleApp());
 }
